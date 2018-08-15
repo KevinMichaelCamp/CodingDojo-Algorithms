138a16a6e73beb20890dfd73c1664967ae22e457
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-15 16:11:51 -0500

    Claire Algo

---
+++ Chapter2/19_Claire_is_Where.js
@@ -0,0 +1,3 @@
+/*  On New Year's, have fun but don't forget your way home! For this challenge create four functions (reset, moveBy, xLocation, and yLocation) to track the travels of Claire, a wanderer.  Calling reset() moves Claire home to the origin (0,0).  The moveBy(xOffset, yOffset) function moves her by those amounts, in those directions.  Finally, xLocation() and yLocation() return how far Claire is from home, in X and Y directions respectively.  After the calls of reset(), moveBy(1,-2), and moveBy(3,1), subsequently calling xLocation() and yLocation() should return 4 and -1.
+
+Second: Create function distFromHome().  Assuming she moves diagonally, reutrn her distance from home. */
