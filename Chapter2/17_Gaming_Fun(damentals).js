d89aa07fa18908f356dde323c50430976ae2664a
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-15 10:56:56 -0500

    Coding Dojo Algos - 08.15.2018

---
+++ Chapter2/17_Gaming_Fun(damentals).js
@@ -0,0 +1,7 @@
+/* It's New Year's Eve, so let's play some dicec games!  It'll be fun.  What could go wrong?
+
+1. Create function rollOne() to return a randomly selected integer between 1 and 6 (inclusive).
+
+2. Second, create a function playFives(num), which should call rollOne() multiple times - 'num' times, in fact, where 'num' is input parameter to playFives(num).  Each time, it should print the value rollOne() returns, and if that roll value is 5, also print "That's good luck !".
+
+3.
--- Chapter2/time.js
+++ Chapter2/time.js
@@ -1,39 +1,17 @@
-//  DIFFICULT!!!(for me - 08.14.2018)
-
-
-/* Regardless of how hard a Dojo student works (and they should work hard), they need time now and then to unwind - like hands on a clock.  Traditional clocks are increasingly uncommon, but most can still read an analog clock's hands of hours, minutes, and seconds.  Create clockHandAngles(seconds) that, given a number of seconds since 12:00:00, prints the angle (in degrees) of the hour, minute, and second hands. For review: 360° form a full rotation.  For input of 3600 seconds (equivalent to 1:00:00), print "Hour hand: 30° - Minute hand: 0° - Second hand: 0°."  For an input parameter of 119730 (which is equivalent to 9:15:30 plus 24 hours!), you should log "Hour hand: 277.745° - Minute hand: 93° - Second hand: 180°."  Note: in the second example, the angle for the minute hand is not simply 90°; it has advanced a bit further, because of the additional 30 seconds in that minute so far.
-
-Second: also calculate and print degrees for an additional "week hand" that rotates once each week. */
-
 function clockHandAngles(seconds){
 
-  var time = seconds;
   var wks;
   var hrs;
   var min;
   var sec;
 
-  //Week Hand
-
-  wks = time / 604800;
-  time = time % 604800;
-
-  //Hour Hand
-  if(time < 604800){
-    hrs = time / 3600;
-    time = time % 3600;
-  }
-
-  //Minute Hand
-  if(time < 3600){
-    min = time / 60;
-    sec = time % 60;
-  }
-
-  //Second Hand
-  if(time < 60){
-    sec = time;
-  }
+  wks = seconds / 604800;
+  seconds = seconds % 604800;
+  hrs = seconds / 3600;
+  seconds = seconds % 3600;
+  min = seconds / 60;
+  sec = seconds % 60;
+  sec = seconds;
 
   //Make Time Objects for table
   var weeks = {
