c9057bd84a25229ec9d5e9847d0bae4f045cf84d
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-16 08:40:27 -0500

    Coding Dojo Algo Initial 08.16.2018

---
+++ Chapter2/20_Date_on_a _Deserted_Island.js
@@ -0,0 +1,7 @@
+/* After a particuliarly fabulous New Year's Eve party to end 2018, Eduardo wakes up to find himself stranded on a deserted island.  He misses home in Burbank, but at least now he can spend plenty of time outdoors - and you can't beat the commute!  To pass the time until he is rescued, he counts the sunrises.
+
+1. Help Eduardo track what day of the week it is.  Create a weekdayName(weekdayNum) function that, given a number between 1 & 7, will console.log a string containing the day of the week for that number (given 1, log "Sunday").  Use a SWITCH statement.
+
+2. Expand weekdayName() to create weekdayName2(dayNum) accepting numbers up to 365.  Return weekday as before, given number of days total.  "Sunday" still corresponds to 1.
+
+3. 
