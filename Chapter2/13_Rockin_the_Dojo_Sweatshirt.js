b6195eda224cbb933e1e2fb64122a28b39f1b4d3
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-17 12:39:11 -0500

    Coding Dojo Algos - 08.17.2018

--- Chapter2/13_Rockin_the_Dojo_Sweatshirt.js
+++ Chapter2/13_Rockin_the_Dojo_Sweatshirt.js
@@ -1,19 +1,15 @@
 /* Ever since you arrived at the Dojo, you wanted one of those Coding Dojo sweatshirts - maybe more than one.  Let's say they cost $20 (including tax), but friendly Josh gives a 9% discount if you buy two, a nice 19% discount if you buy three, or a sweet 35% discount if you buy four or more.  He only accepts cash and says he doesn't have coins, so you should round up to the nearest dollar.  Build a function sweatshirtPricing(num) that, given a number of sweatshirt, returns the cost. */
 
-function sweatshirtPricing(num){
+function sweatshirtPricing(num) {
   var price = 20;
   var discount = 0;
   var total;
 
-  if(num = 1){
-  }
-  else if(num = 2){
+  if (num == 1) {} else if (num == 2) {
     discount = (20 * .09) * num;
-  }
-  else if(num = 3){
+  } else if (num == 3) {
     discount = (20 * .19) * num;
-  }
-  else{
+  } else {
     discount = (20 * .35) * num
   }
   total = price * num - discount;
