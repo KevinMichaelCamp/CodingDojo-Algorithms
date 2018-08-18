90b5606373fca70c6c0ab65b5fb499466729e332
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-18 08:57:26 -0500

    20 Sided Die

--- Chapter2/18_Statistics_Until_Double.js
+++ Chapter2/18_Statistics_Until_Double.js
@@ -1,1 +1,13 @@
 /* Here's another game for New Year's Eve party.  Implement a '20-sided die' that randomly returns integers between 1 and 20 (inclusive).  Roll these, tracking statistics until you get a value twice in a row.  Display number of rolls, min, max, and average. */
+
+function twentySidedDie() {
+  var roll;
+
+  roll = Math.trunc(Math.random() * (20 - 1 + 1) + 1);
+  console.log(roll);
+}
+
+//Test Case
+for (var i = 1; i <= 100; i++) {
+  twentySidedDie();
+}
