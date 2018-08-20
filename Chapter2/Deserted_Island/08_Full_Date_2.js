a6f85508c70cc335b5878aa92ff7979f2e321888
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-19 20:07:13 -0500

    Still working?

--- Chapter2/Deserted_Island/08_Full_Date_2.js
+++ Chapter2/Deserted_Island/08_Full_Date_2.js
@@ -9,13 +9,17 @@ function fullDate2(dayNum){
   var month;
   var day;
   var date;
+  var leapYear = false;
 
   // Edge Cases - make sure input is within 1-365 range
   if (dayNum < 1) {
     return "Day number must be a positive integer."
   }
 
-
+  // Check for leap year
+  if(year % 4 == 0){
+    leapYear = true;
+  }
 
   // Find Month
   function dayToMonth(dayRem) {
