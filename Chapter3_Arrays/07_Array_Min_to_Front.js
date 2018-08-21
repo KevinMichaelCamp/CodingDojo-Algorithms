8b1621008356973ae40e17a84d981672f094bffb
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-21 08:08:29 -0500

    Min to front instructions

---
+++ Chapter3_Arrays/07_Array_Min_to_Front.js
@@ -0,0 +1,1 @@
+/* Given an array of comparable values, move the lowest element to array's front, shifting backward any elements previously ahead of it.  Do not otherwise change the array's order.  Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it.  As always, do this without using built-in functions. */
