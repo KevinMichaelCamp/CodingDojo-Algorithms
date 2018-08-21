10502131b840fc0d6bfb5e6e0ae69a2a069493d8
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-21 15:11:05 -0500

    Skyline Heights

---
+++ Chapter3_Arrays/12_Skyline_Heights.js
@@ -0,0 +1,8 @@
+/* Lovely Burbank has a breathtaking view of the Los Angeles skyline.  Let's say you are given an array with heights of consecutive buildings, starting closest to you and extending away.  Array [-1,7,3] would represent three buildings: first is actually out of view of street level, behind it is second at 7 stories high, and the third is 3 stories high (behind the 7-story).  You are situated at street level.  Return array containing heights of buildings you can see, in order.  Given [-1,1,1,7,3] return [1,7].  Given [0,4] return [4].  As always with challenges, do not use any built-in array functions such as unshift(). */
+
+function skylineHeights(arr) {
+
+}
+
+//Test Cases
+console.log("Test Case1");
