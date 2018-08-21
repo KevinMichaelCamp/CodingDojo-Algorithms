67f45093a9ca771d65e3daef430282f8ba39c9dd
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-20 21:02:07 -0500

    Instructions

---
+++ Chapter3_Arrays/04_Array_Remove_At.js
@@ -0,0 +1,1 @@
+/* Given array and an index into array, remove and return the array value at that index.  Do this without using built-in array methods except pop().  Think of popFront(arr) as equivalent to removeAt(arr, 0). */
