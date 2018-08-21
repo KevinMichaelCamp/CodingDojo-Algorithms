a0b37cd1ec27c2ff2573f0bbda00e8d122b589c1
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-21 13:21:39 -0500

    Array rotate

---
+++ Chapter3_Arrays/09_Array_Rotate.js
@@ -0,0 +1,15 @@
+/* Implement arrayRotate(arr, shiftBy) that accepts array and offset.  Shift array's values to the right by that amount.  "Wrap-around" any values that shift off the array's end to the other side, so that no data is lost in the process.  Operate in-place: given ([1,2,3], 1), change array to [3,1,2].  Don't use built-in functions.
+
+Second: Allow negative shiftBy (shift left, instead of right).
+
+Third: Minimize memory usage.  With no new array, handle arrays/shiftBys in the millions.
+
+Fourth: Minimize the touches of each element. */
+
+function arrayRotate(arr, shiftBy){
+
+
+}
+
+//Test Cases
+console.log("Test Case 1");
