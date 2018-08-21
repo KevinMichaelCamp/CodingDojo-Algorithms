0369e1dd8da27b74dc8a7bf5c0da2e8403875083
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-20 21:22:25 -0500

    More instructions

---
+++ Chapter3_Arrays/05_Array_Swap_Pairs.js
@@ -0,0 +1,1 @@
+/* Swap positions of successive pairs of values of given array.  If length is odd, do not change the final element.  For [1,2,3,4], return [2,1,4,3].  For example, change input ["Brendan", true, 42] to [true, "Brendan", 42].  As with all array challenges, do this without using any built-in array methods. */
