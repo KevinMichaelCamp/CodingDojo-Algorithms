f29cc5cf7a7fd1675cd5b92491c929afc198c29f
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-21 15:24:59 -0500

    Last commit# ------------------------ >8 ------------------------ # Do not touch the line above. # Everything below will be removed. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed: # # 	modified:   Chapter3_Arrays/16_Array_Nth_to_Last.js

---
+++ Chapter3_Arrays/16_Array_Nth_to_Last.js
@@ -0,0 +1,8 @@
+/* Return th eelement that is N-from-array's-end.  Given ([5,2,3,6,4,9,7], 3), return 4.  If the array is too short, return null. */
+
+function arrayNthToLast(arr, num) {
+
+}
+
+//Test Cases
+console.log("Test Case1");
