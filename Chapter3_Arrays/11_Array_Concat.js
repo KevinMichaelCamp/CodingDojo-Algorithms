e72e986e047b5d933e5d99dd2f2c10430e164541
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-21 15:05:11 -0500

    Mas instrucciones

---
+++ Chapter3_Arrays/11_Array_Concat.js
@@ -0,0 +1,8 @@
+/* Replicate JavaScript's concat().  Create a standalone function that accepts two arrays.  Return a new array containing the first array's elements, followed by the second array's elements.  Do not alter the original arrays.  Example: arrayConcat(["a","b"], [1,2]) should return new array ["a","b",1,2]. */
+
+function arrayConcat(arr1, arr2) {
+
+}
+
+//Test Cases
+console.log("Test Case1");
