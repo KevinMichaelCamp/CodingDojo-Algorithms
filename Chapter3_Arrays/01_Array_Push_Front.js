a01f49a46f4a67cf183a3380d65a53dd52d5ebe9
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-20 20:31:40 -0500

    More arrays

--- Chapter3_Arrays/01_Array_Push_Front.js
+++ Chapter3_Arrays/01_Array_Push_Front.js
@@ -1,9 +1,11 @@
 /* Given array and an additional value, insert this value at the beginning of the array.  Do this without using any built in array methods. */
 
 function arrayPushFront(arr, val) {
+
   for (var i = arr.length; i >= 0; i--) {
     arr[i] = arr[i - 1];
   }
+
   arr[0] = val;
   console.log(arr);
 }
@@ -15,8 +17,14 @@ var val1 = 1;
 arrayPushFront(arr1, val1);
 console.log("********************");
 
-console.log("Test Case 1");
+console.log("Test Case 2");
 var arr2 = [-1, 2, -3, 4, -5];
 var val2 = 0;
 arrayPushFront(arr2, val2);
+console.log("********************");
+
+console.log("Test Case 3");
+var arr3 = ["my", "name", "is", "Kevin"];
+var val3 = "Hello";
+arrayPushFront(arr3, val3);
 console.log("********************");
--- Chapter3_Arrays/02_Array_Pop_Front.js
+++ Chapter3_Arrays/02_Array_Pop_Front.js
@@ -1,1 +1,23 @@
 /* Given array, remove and return the value at the beginning of the array.  Do this without using any built-in array methods except pop(). */
+
+function arrayPopFront(arr) {
+  var temp = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i + 1];
+  }
+
+  arr[arr.length - 1] = temp;
+  return arr.pop();
+}
+
+//Test Cases
+console.log("Test Case 1");
+var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arrayPopFront(arr1));
+console.log("********************");
+
+console.log("Test Case 2");
+var arr2 = [-245, 325, -21, 52];
+console.log(arrayPopFront(arr2));
+console.log("********************");
