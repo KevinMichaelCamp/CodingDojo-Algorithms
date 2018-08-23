30db2d41ce1285c6d301c9dcc7e972a85efb060a
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-23 08:44:23 -0500

    Rotate array

--- Chapter3_Arrays/09_Array_Rotate.js
+++ Chapter3_Arrays/09_Array_Rotate.js
@@ -7,9 +7,29 @@ Third: Minimize memory usage.  With no new array, handle arrays/shiftBys in the 
 Fourth: Minimize the touches of each element. */
 
 function arrayRotate(arr, shiftBy) {
+  var temp = arr[arr.length - 1];
 
+  while (shiftBy > 0) {
 
+    for (var i = arr.length - 1; i >= 0; i--) {
+      arr[i] = arr[i - 1];
+    }
+
+    arr[0] = temp;
+    temp = arr[arr.length - 1];
+    shiftBy--;
+  }
+  console.log(arr);
 }
 
 //Test Cases
 console.log("Test Case 1");
+var arr1 = [1, 2, 3];
+var shift1 = 1;
+arrayRotate(arr1, shift1);
+console.log("********************");
+
+console.log("Test Case 2");
+var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var shift2 = 4;
+arrayRotate(arr2, shift2);
