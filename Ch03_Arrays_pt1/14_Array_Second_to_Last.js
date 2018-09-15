c43614e0301fe46877076ec951fec4759e049397
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-15 16:44:26 -0500

    array second to last

--- Ch03_Arrays_pt1/14_Array_Second_to_Last.js
+++ Ch03_Arrays_pt1/14_Array_Second_to_Last.js
@@ -6,8 +6,16 @@ function arraySecondToLast(arr) {
     return null;
   }
 
-  for(var i = 0; i )
+  console.log(arr[arr.length - 2])
 }
 
 //Test Cases
 console.log("Test Case1");
+var arr1 = [42,true,4,"Kate",7];
+arraySecondToLast(arr1);
+console.log("********************")
+
+console.log("Test Case2");
+var arr1 = [1,2,3,4,5,6,7,8,9];
+arraySecondToLast(arr1);
+console.log("********************")
