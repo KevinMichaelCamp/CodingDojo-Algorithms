dd5cf6243e14581860754db1499a78695dffd6ad
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-18 17:09:31 -0500

    Algos 09.18.2018

--- Ch03_Arrays_pt1/13_Array_Remove_Negatives.js
+++ Ch03_Arrays_pt1/13_Array_Remove_Negatives.js
@@ -1,8 +1,23 @@
+
+FIX ME
 /* Implement arrayRemoveNegatives() that accepts array, removes negative values, and returns the same array (not a copy), preserving the non-negative values' order.  As always, do not use built-in array functions. */
 
 function arrayRemoveNegatives(arr) {
+  var temp;
 
+  console.log(arr);
+
+  for(var i = 0; i < arr.length; i ++){
+    if(arr[i] < 0){
+      temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+    }
+    console.log(arr);
+  }
 }
 
 //Test Cases
 console.log("Test Case1");
+var arr1 = [1,-2,3,4,5,6];
+arrayRemoveNegatives(arr1);
