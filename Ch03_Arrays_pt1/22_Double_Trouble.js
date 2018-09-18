02348b58a12d092f733726247663fc341136d40d
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-18 17:34:15 -0500

    22_Double_Trouble

---
+++ Ch03_Arrays_pt1/22_Double_Trouble.js
@@ -0,0 +1,1 @@
+/* Create a function that changes a given array to list each original element twice, retaining original order.  Convert [4,"Ulysses",42,false] to [4,4,"Ulysses","Ulysses",42,42,false,false]. */
