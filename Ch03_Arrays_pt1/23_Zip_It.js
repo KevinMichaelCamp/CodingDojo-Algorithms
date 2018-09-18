60a12a6cbbd656006d40f81206d089b636072ae9
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-18 17:37:03 -0500

    23_Zip_It

---
+++ Ch03_Arrays_pt1/23_Zip_It.js
@@ -0,0 +1,3 @@
+/* Create a standalone function that accepts two arrays and combines their values sequentially into a new array, at alternating indices starting with the first array.  Extra values from either array should be included afterward.  Given [1,2] and [10,20,30,40], return new array containing [1,10,2,20,30,40].
+
+Second: combine the two arrays' values into the first array, instead of into a new array.  Much more fun!!*/
