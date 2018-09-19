c3632e751684e2260bbae00b6479e37e655219d2
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-19 13:05:07 -0500

    02_String_Get_Digits

---
+++ Ch04_Strings_and_Associative_Arrays/02_String_Get_Digits.js
@@ -0,0 +1,1 @@
+/* Create a JavaScript function that given a string, returns the integer made from that string's digits.  Given "0s1a3y5w7h9a2t4?6!8?0", the function shouyld return the number 1357924680. */
