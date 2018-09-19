2d7121b71793c4502c24d730a74330056d873a6b
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-19 16:57:04 -0500

    07_Remove_Even_Length_Strings

---
+++ Ch04_Strings_and_Associative_Arrays/07_Remove_Even_Length_Strings.js
@@ -0,0 +1,1 @@
+/* Build a standalone function to remove strings of even lengths from a given array.  For array containing ["Nope!", "Its", "Kris", "starting", "with", "K!", "(instead", "of", "Chris", "with","C)","."], change that same array to ["Nope!", "Its", "Chris","."]. */
