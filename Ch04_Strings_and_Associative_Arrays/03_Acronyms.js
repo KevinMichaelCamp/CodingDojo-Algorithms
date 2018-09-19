38804ec48c59aebc0b4342ca969219299d91ab09
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-19 13:07:50 -0500

    03_Acronyms

---
+++ Ch04_Strings_and_Associative_Arrays/03_Acronyms.js
@@ -0,0 +1,1 @@
+/* Create a function that given a string, returns the string's acronym (first letters only, capitalized).  Given " there's no free lunch - gotta pay yer way.", return "TNFL-GPYW".  Given "Live from New York, it's Saturday Night!", return "LFNYISN". */
