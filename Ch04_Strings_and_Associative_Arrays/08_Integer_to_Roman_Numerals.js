3cb751aa29302d5e5ac2352f325fbf2eeaa67271
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-09-20 20:26:32 -0500

    08_Integer_to_Roman_Numerals

---
+++ Ch04_Strings_and_Associative_Arrays/08_Integer_to_Roman_Numerals.js
@@ -0,0 +1,1 @@
+/* Given a positive integer that is less than 4000, return a string containing that value in Roman numeral representation, I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000.  Remember that 4 is IV, 349 is CCCIL, and 444 is CDXLIV. */
