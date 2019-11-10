19cb595fe6a354aa9b96467c69d0f3d46991aca9
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-11-10 16:56:15 -0600

    algo

--- Ch04_Strings_and_Associative_Arrays/07_Remove_Even_Length_Strings.js
+++ Ch04_Strings_and_Associative_Arrays/07_Remove_Even_Length_Strings.js
@@ -1,18 +1,18 @@
 /* Build a standalone function to remove strings of even lengths from a given array.  For array containing ["Nope!", "Its", "Kris", "starting", "with", "K!", "(instead", "of", "Chris", "with","C)","."], change that same array to ["Nope!", "Its", "Chris","."]. */
-function removeEvenLength(arr){
-  for(var i = 0; i < arr.length; i ++){
-    if(arr[i].length % 2 == 0){
+function removeEvenLength(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length % 2 == 0) {
       var temp = arr[i];
-      for(var j = i; j < arr.length; j ++){
-        arr[j] = arr[j+1];
+      for (var j = i; j < arr.length; j++) {
+        arr[j] = arr[j + 1];
       }
-      arr[arr.length - 1 ] = temp;
+      arr[arr.length - 1] = temp;
       arr.pop()
-      i --;
+      i--;
     }
   }
   return arr
 }
 
 
-console.log(removeEvenLength(["Nope!", "Its", "Kris", "starting", "with", "K!", "(instead", "of", "Chris", "with","C)","."]));
+console.log(removeEvenLength(["Nope!", "Its", "Kris", "starting", "with", "K!", "(instead", "of", "Chris", "with", "C)", "."]));
--- Ch04_Strings_and_Associative_Arrays/08_Integer_to_Roman_Numerals.js
+++ Ch04_Strings_and_Associative_Arrays/08_Integer_to_Roman_Numerals.js
@@ -1,1 +1,30 @@
 /* Given a positive integer that is less than 4000, return a string containing that value in Roman numeral representation, I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000.  Remember that 4 is IV, 349 is CCCIL, and 444 is CDXLIV. */
+
+function integerToRomanNum(num) {
+    var roman = "";
+    var lookup = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+
+    for (i in lookup) {
+        while (num >= lookup[i]) {
+            roman += i;
+            num -= lookup[i];
+        }
+    }
+    return roman;
+}
+
+console.log(integerToRomanNum(4562));
