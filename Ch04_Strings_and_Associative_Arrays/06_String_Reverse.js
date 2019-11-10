a2e39e5a1972f8bf639ae7654da10b70770a0f1f
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-11-10 16:47:48 -0600

    algo

--- Ch04_Strings_and_Associative_Arrays/06_String_Reverse.js
+++ Ch04_Strings_and_Associative_Arrays/06_String_Reverse.js
@@ -1,1 +1,11 @@
 /* Implement reverseString(str) that, given a string, returns that string with characterss reversed .  Given "creature", return "erutaerc".  Tempting as it seems, do not use the built-in reverse()! */
+
+function reverseString(str) {
+    var newStr = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+
+console.log(reverseString("creature"));
