9deb495b09bc14c7c010eb039c79600df28a5007
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-12-11 18:08:03 -0600

    shuffle array

--- Ch03_Arrays_pt1/19_Array_Shuffle.js
+++ Ch03_Arrays_pt1/19_Array_Shuffle.js
@@ -1,1 +1,20 @@
 /* In JavaScript, the Array object has numerous useful methods.  It does not, however, contain a method that will randomize the order of an array's elements.  Let's create shuffle(arr), to efficiently shuffle a given array's values.  Work in-place, naturally.  Do you need to return anything from your function? */
+
+function shuffleArray(arr) {
+    let curr_idx = arr.length;
+    let temp, rand_inx;
+
+    while (curr_idx !== 0) {
+        rand_inx = Math.floor(Math.random() * curr_idx);
+        curr_idx--;
+        temp = arr[curr_idx];
+        arr[curr_idx] = arr[rand_inx];
+        arr[rand_inx] = temp;
+    }
+
+    return arr;
+}
+
+// Test Cases
+myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(shuffleArray(myArr));
